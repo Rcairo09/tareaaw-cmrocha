65e74ce0c0768e883d463c96329a45401246cc3d
Cairo <rcairor09@gmail.com>
2022-10-01 23:16:46 -0600

    Tercer ejercicio

--- 02 Ejercicio.sql
+++ 02 Ejercicio.sql
@@ -1,10 +1,16 @@
---Crea una vista que muestre un listado de productos (Production.Product) activos con sus respectivas categorías (Production.ProductCategory), subcategorías (Production.ProductSubcategory)
---y modelo (Production.ProductModel).Deben mostrarse todos los productos activos aunque no tengan modelo asociado.
-
 USE AdventureWorks2019
 GO
 
-SELECT 
-FROM Production.Product p
-INNER JOIN Production.ProductModel m 
-ON p.ProductID = m.ProductModelID 
+IF OBJECT_ID ('Production.ProductosCategoria', 'V') IS NOT NULL  
+DROP VIEW Production.ProductosCategoria;  
+GO
+
+CREATE VIEW Production.ProductosCategoria
+AS
+SELECT p.ProductID, p.Name AS Producto, s.Name AS Subcategoria, c.Name AS Categoria, m.Name AS Modelo
+FROM Production.Product p, Production.ProductSubcategory s, Production.ProductCategory c, Production.ProductModel m
+GO
+
+SELECT *
+FROM Production.ProductosCategoria
+GO
