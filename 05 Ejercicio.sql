89a6c1316c5dbf266892e928e801c1235e3c6fac
Cairo <rcairor09@gmail.com>
2022-10-02 21:43:00 -0600

    ejercicio 5 y 6

---
+++ 05 Ejercicio.sql
@@ -0,0 +1,23 @@
+IF EXISTS (
+  SELECT * 
+    FROM INFORMATION_SCHEMA.ROUTINES 
+   WHERE SPECIFIC_SCHEMA = N'HumanResources'
+     AND SPECIFIC_NAME = N'usp_GetBirthDate' 
+)
+   DROP PROCEDURE HumanResources.usp_GetBirthDate
+GO
+
+CREATE PROCEDURE HumanResources.usp_GetBirthDate
+	@DepartmentId INT = NULL
+AS
+	SELECT p.FirstName AS Nombre, p.LastName AS Apellido, e.Gender, d.Name AS Departamento, e.BirthDate
+	FROM Person.Person p
+	INNER JOIN HumanResources.Employee e ON p.BusinessEntityID = e.BusinessEntityID
+	INNER JOIN HumanResources.EmployeeDepartmentHistory h ON e.BusinessEntityID = h.BusinessEntityID
+	INNER JOIN HumanResources.Department d ON h.DepartmentID = d.DepartmentID
+	WHERE @DepartmentId IS NULL OR @DepartmentId = d.DepartmentID AND DATEPART(MONTH, e.BirthDate) = DATEPART(MONTH, GETDATE())
+	ORDER BY Apellido, Departamento ASC
+GO
+
+EXEC HumanResources.usp_GetBirthDate 
+
--- 06 Ejercicio.sql
+++ 06 Ejercicio.sql
@@ -5,23 +5,21 @@ IF EXISTS (
   SELECT * 
     FROM INFORMATION_SCHEMA.ROUTINES 
    WHERE SPECIFIC_SCHEMA = N'HumanResources'
-     AND SPECIFIC_NAME = N'usp_GetInfo' 
+     AND SPECIFIC_NAME = N'usp_GetGeneralEmployee' 
 )
-   DROP PROCEDURE HumanResources.usp_GetInfo
+   DROP PROCEDURE HumanResources.usp_GetGeneralEmployee
 GO
 
-CREATE PROCEDURE HumanResources.usp_GetInfo
+CREATE PROCEDURE HumanResources.usp_GetGeneralEmployee
 	@DepartmentId INT = NULL
 AS
-	SELECT p.BusinessEntityID AS ID, p.FirstName AS Nombre, p.LastName AS Apellido, 
-	e.BirthDate, e.Gender, h.DepartmentID AS IdDepartamento, d.Name AS Departamento
+	SELECT COUNT (p.FirstName) AS CantidadEmpleados 
 	FROM Person.Person p
 	INNER JOIN HumanResources.Employee e ON p.BusinessEntityID = e.BusinessEntityID
 	INNER JOIN HumanResources.EmployeeDepartmentHistory h ON e.BusinessEntityID = h.BusinessEntityID
 	INNER JOIN HumanResources.Department d ON h.DepartmentID = d.DepartmentID
-	WHERE @DepartmentId IS NULL OR @DepartmentId = d.DepartmentID
-	ORDER BY d.DepartmentID ASC
+	WHERE @DepartmentId IS NULL OR @DepartmentId = d.DepartmentID 
+	
 GO
 
-EXECUTE HumanResources.usp_GetInfo 15
-GO
+EXEC HumanResources.usp_GetGeneralEmployee 
