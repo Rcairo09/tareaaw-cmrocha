433801450d2eabf4387bb0c4584603939fec857e
Cairo <rcairor09@gmail.com>
2022-10-05 18:39:53 -0600

    ejercicio 9 modificado

--- 09 Ejercicio.sql
+++ 09 Ejercicio.sql
@@ -5,7 +5,7 @@ IF OBJECT_ID (N'Sales.ufn_VentasAnuales') IS NOT NULL
    DROP FUNCTION Sales.ufn_VentasAnuales
 GO
 
-CREATE FUNCTION Sales.ufn_VentasAnuales(@Year date = NULL)
+CREATE FUNCTION Sales.ufn_VentasAnuales(@Year int = NULL)
 RETURNS @TablaVentas TABLE(
 		ProductID INT,
 		ProductName NVARCHAR(50),
@@ -17,18 +17,17 @@ RETURNS @TablaVentas TABLE(
 AS
 BEGIN 
 	IF(@Year IS NULL)
-	SET @Year= GETDATE();
+	SET @Year= CONVERT(INT, DATEPART(YEAR, GETDATE()), 101);
 	
-
 	INSERT INTO @TablaVentas (ProductID, ProductName, Cantidad, Total, IdOrden, FechaOrden)
 	SELECT s.ProductID, p.Name, s.OrderQty, s.LineTotal, S.SalesOrderID, h.OrderDate
 		FROM Sales.SalesOrderDetail s
 		INNER JOIN Production.Product p ON S.ProductID = p.ProductID
 		INNER JOIN Sales.SalesOrderHeader h ON s.SalesOrderID = h.SalesOrderID
-		WHERE (DATEPART(YEAR, h.OrderDate) = DATEPART(YEAR, @Year))	
+		WHERE (CONVERT(INT, DATEPART(YEAR, h.OrderDate),101) =  @Year)	
 		RETURN;
 END		
 GO
 
 SELECT *
-FROM Sales.ufn_VentasAnuales('2011-01-01')
+FROM Sales.ufn_VentasAnuales(2014)
